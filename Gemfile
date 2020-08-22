f064ac08ccf3818a3dc033841386676136613f4f
Joel Mikkelsen <joelmikkelsen@macbook-pro.lan>
2020-08-22 17:23:07 -0400

    more oauth

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 # User auth
 gem 'devise'
+gem 'omniauth-google-oauth2'
+gem 'dotenv-rails', groups: [:development, :test]
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_202117) do
+ActiveRecord::Schema.define(version: 2020_08_22_204251) do
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,8 +32,11 @@ ActiveRecord::Schema.define(version: 2020_08_22_202117) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
