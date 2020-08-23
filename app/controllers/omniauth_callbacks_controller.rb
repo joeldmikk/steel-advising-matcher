b243e1cf45fc74c6c4bda3f9cd3bd568cc7b1386
Joel Mikkelsen <joelmikkelsen@macbook-pro.lan>
2020-08-22 22:22:47 -0400

    omniauth working

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,46 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 # replace with your authenticate method
-  skip_before_action :authenticate_user!
+#   skip_before_action :authenticate_user!
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
+    puts "#{auth}"
     user = User.where(provider: auth["provider"], uid: auth["uid"])
             .first_or_initialize(email: auth["info"]["email"])
     user.name ||= auth["info"]["name"]
     user.save!
 
-    user.remember_me = true
     sign_in(:user, user)
 
     redirect_to after_sign_in_path_for(user)
   end
+
+  protected
+
+  def after_omniauth_failure_path_for(_scope)
+    # new_user_session_path
+    new_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
+  end
+
+  private
+
+  def from_google_params
+    @from_google_params ||= {
+      uid: auth.uid,
+      email: auth.info.email,
+      full_name: auth.info.name,
+      avatar_url: auth.info.image
+    }
+  end
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
+
+
+
