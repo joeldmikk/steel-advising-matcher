28b38df37773e245862155606a2cc96316b29da6
Joel Mikkelsen <joelmikkelsen@macbook-pro.lan>
2020-08-26 16:08:32 -0400

    wip

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'dotenv-rails', groups: [:development, :test]
 gem 'react-rails'
 gem 'react-bootstrap-rails'
 
+# allow CORS
+gem 'rack-cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,28 @@ module Template
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins 'http://localhost:3001'
+    #     resource(
+    #       '*',
+    #       headers: :any,
+    #       methods: [:get, :patch, :put, :delete, :post, :options]
+    #       )
+    #   end
+    # end
+
   end
 end
